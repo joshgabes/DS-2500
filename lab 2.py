1bff4338d955a1c92ed1194ec52c0f24e7fb4275
Vwqwq <253540130+Vwqwq@users.noreply.github.com>
2026-09-24 18:17:04 -0400

    1
    Added course description for data science programming course.

---
+++ lab 2.py
@@ -0,0 +1,28 @@
+# LAB EXERCISE 02
+
+# PROBLEM 01
+
+
+# PROBLEM 02
+
+
+# PROBLEM 03
+
+
+# PROBLEM 04
+
+
+### SETUP BEGINS -- DO NOT MODIFY
+course_description = "Offers intermediate to advanced Python programming for data
+science. Covers object oriented design patterns using Python, including
+encapsulation, composition, and inheritance. Advanced programming skills cover
+software architecture, recursion, profiling, unit testing and debugging, lineage
+and data provenance, using advanced integrated development environments, and
+software control systems. Uses case studies to survey key concepts in data science
+with an emphasis on machine learning (classification, clustering, deep learning);
+data visualization; and natural language processing. Additional assigned readings
+survey topics in Ethics, Model Bias, and Data Privacy pertinent to todays Big Data
+world. Offers students an opportunity to prepare for more advanced courses in data
+science and to enable practical contributions to software development and data
+science projects in a commercial setting. "
+### SETUP ENDS -- DO NOT MODIF
